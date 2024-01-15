670ee0e7f1700f82e2168b3702cb708114a75b56
Loïc Laurent <llaurent@selexium.com>
2024-01-15 15:09:00 +0100

    Starter pour plugin WP

---
+++ my_plugin.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ * Plugin Name:       My plugin
+ * Plugin URI:        https://github.com/LoicEsk/docker-dev-env/tree/wordpress-plugin
+ * Description:       Template pour plugin WP
+ * Version:           0.0.0
+ * Author:            LoicEsk
+ * Author URI:        https://github.com/LoicEsk
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// https://make.wordpress.org/core/handbook/best-practices/core-apis/
+// https://developer.wordpress.org/plugins/plugin-basics/best-practices/ 
